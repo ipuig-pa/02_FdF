05c92d57c2fef2c341842f14710acb6a751f3d89
Irene Puig Parnau <ipuig-pa@3-F-3.42heilbronn.de>
2024-11-26 13:06:07 +0100

    advances in mlx function understanding and managing

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 17:29:35 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/11/23 17:00:59 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/11/26 13:05:29 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,44 @@ int	main(void)
 {
 	void	*mlx;
 	void	*mlx_window;
-	void	*img;
+	t_data	img;
 
 	mlx = mlx_init();
+	if (!mlx)
+		return (1);
 	mlx_window = mlx_new_window(mlx, 1920, 1080, "Render");
+	if(!mlx_window)
+	{
+		mlx_destroy_display(mlx);
+		free(mlx);
+		return (1);
+	}
 	mlx_loop(mlx);
-	img = mlx_new_image(mlx, 1920, 1080);
-	
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	if(!img.img)
+	{
+		mlx_destroy_window(mlx_window);
+		mlx_destroy_display(mlx);
+		free(mlx);
+		return (1);
+	}
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	mlx_put_image_to_window();
+	mlx_destroy_window(mlx_window);
+	mlx_destroy_display(mlx);
+	free(mlx);
+	return (0);
 }
 
-parse_map
+// parse_map
 
-draw
+void	my_pixel_put(t_data *img, int x, int y, int color)
+{
+	int		offset;
+	char	*pixel;
+
+	offset = (y * img->line_length + x * (img->bits_per_pixel / 8));
+	pixel = img->addr + offset;
+	*(unsigned int*)pixel = color;
+}
 
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 17:26:24 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/11/23 16:27:45 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2024/11/26 11:38:33 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,15 @@
 # include <mlx.h>
 # include <libft.h>
 # include <ft_printf.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+
 
 #endif
